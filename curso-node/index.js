d7d138be9bb21b63715a4ec89ece699c83f43823
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-07-11 19:56:53 -0300

    Entendendo e manipulando os modulos do Node.Js| Path e Fs

--- curso-node/index.js
+++ curso-node/index.js
@@ -1,9 +1,12 @@
-// Importando um modulo, a class person para o 
+// Importando um modulo, a class person para o
 // Arquivo atual
 const { Person } = require('./person');
 const Emanuel = new Person("Emanuel");
 console.log(Emanuel.sayMyName());
 
 
-// Importando o module path
+// Importando o module Path
 // require('./modules/path');
+
+// Importanto o module Fs
+// require('./modules/fs');
--- curso-node/modules/fs.js
+++ curso-node/modules/fs.js
@@ -1,2 +1,78 @@
+const { error } = require('console');
 const fs = require('fs');
 const path = require('path');
+
+// Criar uma pasta
+fs.mkdir(path.join(__dirname, '/bblioteca'), (error) => {
+    if (error) {
+        return console.log('A pasta já existe', error)
+    }
+});
+
+// Criando um arquivo dentro de uma pasta já existente e escrever nele
+fs.writeFile(
+    path.join(__dirname, '/bblioteca', 'livro1.html'),
+    '<h1>livro 1</h1>', (error) => {
+        if (error) {
+            return console.log('Ocorreu um erro:', error);
+        }
+        console.log("Aquivo criado com sucesso!");
+    });
+
+// Adicionando à um arquivo ou escreve nele
+fs.appendFile(path.join(__dirname, '/bblioteca', 'livro1.html'),
+    ' Escrito por: hello Emanuel',
+    (error) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log('Aquivo alterado com sucesso!');
+    })
+// Ler um arquivo
+fs.readFile(
+    path.join(__dirname, '/bblioteca', 'livro1.html'), "utf-8",
+    (error, data) => {
+        if (error) {
+            return console.log('Ocorreu um erro:', error);
+        }
+        console.log("Aquivo lido com sucesso:", data);
+    }
+)
+
+// Criando a pasta animes
+fs.mkdir(path.join(__dirname, 'Animes'),
+    (erro) => {
+        if (erro) {
+            return console.log('Ocorreu um erro:', erro);
+        }
+        console.log('Pasta criada com sucesso!');
+    });
+// Escrevendo, encrementando e lendo o arquivo da pasta animes;
+fs.writeFile(path.join(__dirname, 'Animes', 'Naruto.html'),
+    "Naruto é o protagonista",
+    (erro) => {
+        if (erro) {
+            return console.log('Ocorreu um erro:', erro);
+        }
+
+        fs.appendFile(path.join(__dirname, 'Animes', 'Naruto.html'),
+            '. Hinata ama o Naruto', (erro) => {
+                if (erro) {
+                    return console.log('Ocooreu um erro: ', erro);
+                }
+                console.log("Arquivo alterado com sucesso");
+            },
+        );
+
+        fs.readFile(path.join(__dirname, 'Animes', 'Naruto.html'),
+            'utf8',
+            (erro, data) => {
+                if (erro) {
+                    return console.log("Ocorreu um erro: ", erro);
+                }
+                console.log(data);
+            }
+        );
+    },
+); 
